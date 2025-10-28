d906df8d56b2928711ee69dd2a624bcdae33f7ec
gkfka30 <gkfka30@gmail.com>
2025-10-28 17:44:00 +0900

    123

--- js/location.js
+++ js/location.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-$(function () {
-  $(function () {
-    // con01, con02 각각에 대해 동일 동작
-    $(".con01, .con02").each(function () {
-      const section = $(this);
-
-      // 각 섹션 안의 버튼들 클릭 시
-      section.find(".btn li").on("click", function (e) {
-        e.preventDefault();
-
-        const index = $(this).index(); // 클릭한 버튼 순서 가져오기
-
-        // 이미지 전환
-        section.find(".right li").hide().eq(index).fadeIn();
-
-        // 텍스트 전환
-        section.find(".txt li").hide().eq(index).fadeIn();
-
-        // 버튼 투명도 효과
-        section.find(".btn li img").fadeTo(300, 0.5);
-        $(this).find("img").fadeTo(300, 1);
-      });
-    });
-  });
-=======
 // DOMContentLoaded: HTML 문서를 모두 읽고 난 후 스크립트를 실행합니다.
 document.addEventListener("DOMContentLoaded", () => {
   /**
@@ -76,5 +50,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // .con02 섹션에 대해 기능 적용
   setupInteractiveSection(".con02");
->>>>>>> develope
 });
